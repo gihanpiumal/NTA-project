d5f7e12072dd7ce194aa295838eb690b1f538daa
Your Namegihan79097 <gihanpiumal12345@gmail.com>
2022-06-20 21:32:48 +0530

    home 2022.06.20

--- client/src/components/layouts/dashBoard/layout/Layout.jsx
+++ client/src/components/layouts/dashBoard/layout/Layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { LayoutHeader } from "../header";
 
+import './Layout.scss'
+
 const Layout = () => {
   return (
     <>
       <LayoutHeader />
-      {/* <div>layout</div> */}
+      <div className="main-back">layout</div>
+
     </>
   );
 };
